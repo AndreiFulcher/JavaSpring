aa7bbe12d9258c58b70e933d32d917c9bc2ecd8f
AndreiFulcher <andreii.fulcher@gmail.com>
2025-05-19 19:13:20 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/andreifulcher/DSList/entities/Game.java
@@ -0,0 +1,123 @@
+package com.andreifulcher.DSList.entities;
+
+import jakarta.persistence.*;
+
+import java.lang.reflect.GenericArrayType;
+import java.util.Objects;
+
+@Entity //Indica que é uma entidade e será mapeada para uma tabela no banco de dados
+@Table(name = "tb_game") //Renomeia a table
+public class Game {
+
+    @Id //Identifica a Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o auto incremento para a Primary Key
+    private Long id;
+    private String title;
+    @Column(name = "game_year") //Renomeia a coluna pois year é uma palavra reservada do SQL
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override//Garante que o equals é igual para objetos iguais
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override//garante que o hashCode é igual para objetos iguais
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
